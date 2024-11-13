5051b758500beb319241e1aa68026b5091d8b9df
beekntr <2023pietcrkshitij033@poornima.org>
2024-11-13 10:04:10 +0530

    routes

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Button } from '../ui/button';
+import { Link } from 'react-router-dom';
 
 function Hero() {
   return (
-    <div className='flex items-center mx-64 gap-9'>
-    <h1 className='font-extrabold text-[50px]'>
-      <span>The Future of Travel:</span><br/> AI Itineraries,Blockchain Security</h1>
+    <div className='flex flex-col items-center mx-56 gap-9'>
+      <h1 className='font-extrabold text-[50px] text-center mt-16'>
+        <span className='text-[#f56551]'>Discover Your Next Adventure</span>
+      </h1>
+      <p className='text-xl text-gray-500 text-center'>
+        Your personal travel assistant
+      </p>
+      <Link to='/create-trip'>
+        <Button>Get Started, It's Free</Button>
+      </Link>
     </div>
   )
 }
