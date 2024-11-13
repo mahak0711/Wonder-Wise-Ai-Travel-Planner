30d15d249d3837cef846b0e9f8894fb7a1c5a9cf
Mahak <128406559+mahak0711@users.noreply.github.com>
2024-11-13 11:02:23 +0530

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import './App.css'
 import { Button } from './components/ui/button'
 import Hero from './components/custom/Hero'
 import CreateTrip from './create-trip/index.jsx'
+import Header from './components/custom/Header'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <Router>
+      <Header/>
       <Routes>
         <Route path="/" element={<Hero />} />
         <Route path="/create-trip" element={<CreateTrip />} /> {/* Add CreateTrip route */}
