2c1e06649c7c4fef724064074496bbcd3defbe55
beekntr <2023pietcrkshitij033@poornima.org>
2024-11-15 14:31:45 +0530

    viteconf

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 // vite.config.js
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5137',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
